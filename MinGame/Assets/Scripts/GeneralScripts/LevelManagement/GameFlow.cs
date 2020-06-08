bed2dc2b47bc7748ca24453ed8e5a581870e25c4
fournetaaron <66569557+fournetaaron@users.noreply.github.com>
2020-06-07 20:08:24 -0500

    Updated GameFlow to add scores together when a level is complete, not override them.

--- MinGame/Assets/Scripts/GeneralScripts/LevelManagement/GameFlow.cs
+++ MinGame/Assets/Scripts/GeneralScripts/LevelManagement/GameFlow.cs
@@ -100,7 +100,7 @@ public class GameFlow : MonoBehaviour
     {
         for (int i = 0; i < scores.Count; ++i)
         {
-            PlayerInfo[i].score = scores[i];
+            PlayerInfo[i].score += scores[i];
         }
 
         levelManager.TransitionComplete += TransitionCompleted;
