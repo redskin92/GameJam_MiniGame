411065f14cfb3264751f43a3827e7c0256791b04
fournetaaron <66569557+fournetaaron@users.noreply.github.com>
2020-08-16 14:21:01 -0500

    Initial commit :)

---
+++ SurvivalGame/Assets/Scripts/Attributes.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attributes : MonoBehaviour
+{
+    private float health;
+    private float attack;
+    private float defense;
+}
