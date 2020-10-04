240973345d571b4393942823fa50fcbd7510bccb
Brandon Rhinier <redskin92@comcast.net>
2020-10-03 21:14:48 -0500

    - Adding shotgun

--- SurvivalGame/Assets/Scripts/Player/Weapons/PlayerWeapon.cs
+++ SurvivalGame/Assets/Scripts/Player/Weapons/PlayerWeapon.cs
@@ -40,10 +40,32 @@ namespace Player.Weapons
         public void Fire(Quaternion playerFace)
         {
             fireRateCooldownTimer = currentWeapon.fireRate;
-            GameObject newBullet = GameObject.Instantiate(currentWeapon.bulletPrefab, this.transform.position, playerFace);
-            newBullet.GetComponent<Bullet>().SetUpBullet();
+			
+			switch(currentWeapon.fireType)
+			{
+				case WeaponInfo.FireType.SINGLE:
+					GameObject newBullet = GameObject.Instantiate(currentWeapon.bulletPrefab, this.transform.position, playerFace);
+					newBullet.GetComponent<Bullet>().SetUpBullet();
+					break;
+				case WeaponInfo.FireType.SPLIT_5:
+					float spread = 60f;
+					float spreadInc = spread / 5;
+
+					Vector3 bulletDir = playerFace.eulerAngles;
+					bulletDir.z += (spread * 0.5f);
+					for(int i = 0; i < 5; ++i)
+					{
+						GameObject spread5Bullet = GameObject.Instantiate(currentWeapon.bulletPrefab, this.transform.position, Quaternion.Euler(bulletDir));
+						spread5Bullet.GetComponent<Bullet>().SetUpBullet();
+
+						bulletDir.z -= spreadInc;
+					}
+					break;
+			}
+
             currentAmmo--;            
         }
+
 
         public bool CheckAmmo()
         {
